e42fed7b9f7c1c868b58b1df875eeba03abc717b
MrByte61 <minecraft.kill666@gmail.com>
2026-05-16 00:48:52 +0300

    Update get-subs.js

--- api/get-subs.js
+++ api/get-subs.js
@@ -1,3 +1,14 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Бэкенд работает!" });
+  // Сервер считывает ID видео, которое отправил плеер
+  const { videoId } = req.query;
+
+  if (!videoId) {
+    return res.status(400).json({ error: "Передайте videoId" });
+  }
+
+  // Робот подтверждает плееру, что он поймал нужное видео
+  res.status(200).json({ 
+    status: "success",
+    msg: `Робот Vercel перехватил видео ${videoId}. Готовлюсь выгружать текст.`
+  });
 }
